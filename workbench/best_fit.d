f359194cbcbf20a5e5930b2be2b93ba704b91882
Anders Petersen <klasbo@gmail.com>
2014-02-28 20:38:27 +0100

    Best fit algo

--- workbench/best_fit.d
+++ workbench/best_fit.d
@@ -8,12 +8,13 @@ import  std.stdio,
 enum Dir {
     UP,
     DOWN,
-    STOP
+    IDLE
 }
 
 struct State {
     int         prevFloor;
     Dir         dir;
+	bool 		moving;
     bool[][]    btnarr;
 }
 
@@ -23,12 +24,13 @@ enum int doorOpenTime = 3;
 void main(){
 
     State[] states;
-    states ~= State(0, Dir.STOP,    [[false, false, false], [false, false, false], [false, false, false], [false, false, false]]);
-    states ~= State(0, Dir.UP  ,    [[true , false, true ], [true , true , true ], [true , true , true ], [false, true , true ]]);
-    states ~= State(2, Dir.DOWN,    [[true , false, false], [false, false, false], [false, true , false], [false, false, false]]);
-    states ~= State(1, Dir.STOP,    [[false, false, false], [false, false, false], [false, true , false], [false, false, false]]);
-    states ~= State(0, Dir.UP  ,    [[false, false, true ], [true , false, false], [false, false, true ], [false, false, false]]);
-    states ~= State(3, Dir.STOP,    [[false, false, false], [true , false, false], [false, false, false], [false, false, false]]);
+    states ~= State(0, Dir.IDLE,	false,  [[false, false, false], [false, false, false], [false, false, false], [false, false, false]]);
+    states ~= State(0, Dir.UP  ,    false,	[[true , false, true ], [true , true , true ], [true , true , true ], [false, true , true ]]);
+    states ~= State(2, Dir.DOWN,    false, 	[[true , false, false], [false, false, false], [false, true , false], [false, false, false]]);
+    states ~= State(1, Dir.IDLE,    false, 	[[false, false, false], [false, false, false], [false, true , false], [false, false, false]]);
+    states ~= State(0, Dir.UP  ,    false, 	[[false, false, true ], [true , false, false], [false, false, true ], [false, false, false]]);
+    states ~= State(3, Dir.IDLE,    false, 	[[false, false, false], [true , false, false], [false, false, false], [false, false, false]]);
+	states ~= State(2, Dir.DOWN,    true, 	[[true,  false, false], [false, false, false], [false, true , false], [false, false, false]]);
 
 
 
@@ -41,8 +43,9 @@ void main(){
         doorOpenTime*6 + travelTime*5,
         doorOpenTime*2 + travelTime*2,
         travelTime + doorOpenTime,
-        doorOpenTime*3 + travelTime*3,
-        travelTime*2 + doorOpenTime
+        doorOpenTime*3 + travelTime*2,
+        travelTime*2 + doorOpenTime,
+		travelTime*4 + doorOpenTime*2
     ]);
 
 
@@ -62,9 +65,17 @@ int completionTime(State s){
         writeln("Order table is empty");
         return 0;
     }
+	
+	int numButtonPresses = s.btnarr.map!(a => a.count(true)).reduce!"a+b";
+	if(s.dir == Dir.IDLE  &&  numButtonPresses > 1){
+		writeln("dir == IDLE and more than one order makes no sense");
+		return int.max;
+	}
 
     int tmp;
 
+	writeln("Prev floor: ", s.prevFloor, ", moving:", s.moving);
+	
     int floorOfTopOrder = (s.btnarr.length.to!int - 1 - s.btnarr.map!any.retro.countUntil(true));
     writeln("floorOfTopOrder: ", floorOfTopOrder);
 
@@ -78,14 +89,24 @@ int completionTime(State s){
     int floorOfBottomUpwardOrder = s.btnarr.map!(a => a[ButtonType.UP]).countUntil(true);
     writeln("floorOfBottomUpwardOrder: ", floorOfBottomUpwardOrder);
 
-
-    tmp = s.btnarr[s.prevFloor+1..$].map!(a => a[ButtonType.DOWN]).countUntil(true);
-    int floorOfClosestDownwardOrderAbove = (tmp == -1 ? -1 : s.prevFloor + 1 + tmp);
+	int floorOfClosestDownwardOrderAbove;
+	if(s.dir == Dir.DOWN  &&  s.moving){
+		tmp = s.btnarr[s.prevFloor..$]  .map!(a => a[ButtonType.DOWN]).countUntil(true);
+		floorOfClosestDownwardOrderAbove = (tmp == -1 ? -1 : s.prevFloor + tmp);
+	} else {
+		tmp = s.btnarr[s.prevFloor+1..$].map!(a => a[ButtonType.DOWN]).countUntil(true);
+		floorOfClosestDownwardOrderAbove = (tmp == -1 ? -1 : s.prevFloor + 1 + tmp);
+	}
     writeln("floorOfClosestDownwardOrderAbove: ", floorOfClosestDownwardOrderAbove);
 
-
-    tmp = s.btnarr[0..s.prevFloor].map!(a => a[ButtonType.UP]).retro.countUntil(true);
-    int floorOfClosestUpwardOrderBelow = (tmp == -1 ? -1 : s.prevFloor - 1 - tmp);
+	int floorOfClosestUpwardOrderBelow;
+	if(s.dir == Dir.UP  &&  s.moving){
+		tmp = s.btnarr[0..s.prevFloor+1].map!(a => a[ButtonType.UP]).retro.countUntil(true);
+		floorOfClosestUpwardOrderBelow = (tmp == -1 ? -1 : s.prevFloor - tmp);
+	} else {
+		tmp = s.btnarr[0..s.prevFloor]  .map!(a => a[ButtonType.UP]).retro.countUntil(true);
+		floorOfClosestUpwardOrderBelow = (tmp == -1 ? -1 : s.prevFloor - 1 - tmp);
+	}
     writeln("floorOfClosestUpwardOrderBelow: ", floorOfClosestUpwardOrderBelow);
 
 
@@ -97,26 +118,38 @@ int completionTime(State s){
     int numStops = numStopsUpward + numStopsDownward + numStopsCommandUnique;
     writeln("numStops: ", numStops);
 
-/+    
-downward:
-    s.prevFloor -> floorOfBottomOrder ->
-    if floorOfTopOrder > s.prevFloor
-        -> floorOfTopOrder -> floorOfClosestDownwardOrderAbove .
-    else
-        -> floorOfClosestUpwardOrderBelow .
-        
-stop:
-    floorOfOnlyOrder = floorOfTopOrder = floorOfBottomOrder
-    s.prevFloor -> floorOfOnlyOrder
-    
-+/
-
-
-    return
-        numStops * doorOpenTime;
-
-
-
+	int completionTime;
+	
+	completionTime += numStops * doorOpenTime;
+	
+	if(s.dir == Dir.DOWN){
+		completionTime += (s.prevFloor - floorOfBottomOrder) * travelTime;
+		if(floorOfTopOrder > s.prevFloor  ||  (s.moving && floorOfTopOrder >= s.prevFloor) ){
+			completionTime += (floorOfTopOrder - floorOfBottomOrder) * travelTime;
+			completionTime += (floorOfTopOrder - floorOfClosestDownwardOrderAbove) * travelTime;
+		} else {
+			if(floorOfClosestUpwardOrderBelow != -1){
+				completionTime += (floorOfClosestUpwardOrderBelow - floorOfBottomOrder) * travelTime;
+			}
+		}
+	}
+	if(s.dir == Dir.UP){
+		completionTime += (floorOfTopOrder - s.prevFloor) * travelTime;
+		if(s.prevFloor > floorOfBottomOrder  ||  (s.moving && s.prevFloor >= floorOfBottomOrder)){
+			completionTime += (floorOfTopOrder - floorOfBottomOrder) * travelTime;
+			completionTime += (floorOfClosestUpwardOrderBelow - floorOfBottomOrder) * travelTime;
+		} else {
+			if(floorOfClosestDownwardOrderAbove != -1){
+				completionTime += (floorOfTopOrder - floorOfClosestDownwardOrderAbove) * travelTime;
+			}
+		}
+	}
+	if(s.dir == Dir.IDLE){
+		assert(floorOfBottomOrder == floorOfTopOrder, "dir == IDLE and more than one order makes no sense");
+		completionTime += (s.prevFloor - floorOfBottomOrder).abs * travelTime;
+	}
+	
+    return completionTime;
 }
 
 
