b5bd93c463c1d2de6f8d341cf292387d225ceee2
Anders Petersen <klasbo@gmail.com>
2019-02-04 01:48:19 +0100

    Add defaults to in-executable config

--- src/elev_config.d
+++ src/elev_config.d
@@ -9,32 +9,32 @@ import elevio.elev_types : ElevatorType;
 import fns.elevator_state : ClearRequestType;
 
 struct Config {
-    ubyte               id;
-    int                 numFloors;
+    ubyte               id                                              = 1;
+    int                 numFloors                                       = 4;
 
-    ElevatorType        elevio_elev_elevtype;
-    string              elevio_elev_ip;
-    ushort              elevio_elev_port;
+    ElevatorType        elevio_elev_elevtype                            = ElevatorType.simulation;
+    string              elevio_elev_ip                                  = "localhost";
+    ushort              elevio_elev_port                                = 15657;
 
-    int                 feeds_elevioReader_pollrate;
+    int                 feeds_elevioReader_pollrate                     = 25;
 
-    ushort              feeds_peerList_port;
-    int                 feeds_peerList_timeout;
-    int                 feeds_peerList_interval;
+    ushort              feeds_peerList_port                             = 16567;
+    int                 feeds_peerList_timeout                          = 550;
+    int                 feeds_peerList_interval                         = 50;
 
-    ushort              feeds_elevatorStates_port;
-    size_t              feeds_elevatorStates_bufSize;
-    int                 feeds_elevatorStates_minPeriod;
+    ushort              feeds_elevatorStates_port                       = 16568;
+    size_t              feeds_elevatorStates_bufSize                    = 1024;
+    int                 feeds_elevatorStates_minPeriod                  = 100;
 
-    ushort              feeds_requestConsensusCab_port;
-    ushort              feeds_requestConsensusHall_port;
-    size_t              feeds_requestConsensus_bufSize;
-    int                 feeds_requestConsensus_minPeriod;
-    int                 feeds_requestConsensus_maxPeriod;
+    ushort              feeds_requestConsensusCab_port                  = 16569;
+    ushort              feeds_requestConsensusHall_port                 = 16570;
+    size_t              feeds_requestConsensus_bufSize                  = 1024;
+    int                 feeds_requestConsensus_minPeriod                = 100;
+    int                 feeds_requestConsensus_maxPeriod                = 150;
 
-    int                 feeds_elevatorControl_doorOpenDuration;
-    int                 feeds_elevatorControl_travelTimeEstimate;
-    ClearRequestType    feeds_elevatorControl_clearRequestType;
+    int                 feeds_elevatorControl_doorOpenDuration          = 3000;
+    int                 feeds_elevatorControl_travelTimeEstimate        = 2500;
+    ClearRequestType    feeds_elevatorControl_clearRequestType          = ClearRequestType.inDirn;
 }
 
 
